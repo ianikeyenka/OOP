bc283e9a32424b1e2ca048811b0747a65e57f148
IlyaAnik <71770143+IlyaAnik@users.noreply.github.com>
2021-11-03 22:42:18 +0300

    Add files via upload

---
+++ ЛР№1/lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab1
+{
+    class A1
+    {
+        private int a = 12;
+        private int b = 5;
+        private int c;
+        public int C
+        {
+            get
+            {
+                return a %= b;
+            }
+
+        }
+
+    }
+
+    class A2
+    {
+        private int a = 13;
+        private int b = 15;
+        public int C
+        {
+            get
+            {
+                return a + b;
+            }
+
+        }
+    }
+    class Programm
+    {
+        static void Main()
+        {
+            A1 f = new A1();
+            int c1 = f.C;
+            Console.WriteLine($"При %= с равняется: {c1}");
+
+            A2 f1 = new A2();
+            int c2 = f1.C;
+            Console.WriteLine($"При + с равняется: {c2}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
