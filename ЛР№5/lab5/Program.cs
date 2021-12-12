090af83df5d00301fbe6b6782cd312c6cb71fb43
Ilya Anikeyenka <71770143+IlyaAnik@users.noreply.github.com>
2021-12-13 01:24:35 +0300

    Add files via upload

---
+++ ЛР№5/lab5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lab5
+{
+    public class A
+    {
+        private int x;
+        public A(int x) { this.x = x; }
+        public static bool operator true(A obj) => obj.x > 0;
+        public static bool operator false(A obj) => obj.x <= 0;
+        public static bool operator !(A obj)
+        {
+            if(obj)
+                return false;
+            else
+                return true;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            A objectA = new(5);
+            A objectB = new(0);
+            if (objectA) Console.WriteLine("True");
+            else Console.WriteLine("False");
+
+            if (!objectA) Console.WriteLine("True");
+            else Console.WriteLine("False");
+
+            if (objectB) Console.WriteLine("True");
+            else Console.WriteLine("False");
+
+            if (!objectB) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
