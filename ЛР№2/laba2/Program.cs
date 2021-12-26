1b4933e0a4a96126d7e3857eae54fd6bd820b7bb
Ilya Anikeyenka <71770143+IlyaAnik@users.noreply.github.com>
2021-12-26 19:56:22 +0300

    Update Program.cs

--- ЛР№2/laba2/Program.cs
+++ ЛР№2/laba2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace lab2
 {
@@ -9,22 +9,22 @@ namespace lab2
             this.a = a;
             this.b = b;
         }
-        
+
         private int a;
         private int b;
 
-        public int C{get{return (a + b); }}
+        public int C { get { return (a + b); } }
     }
 
     class B : A
     {
         public B(int a, int b) : base(a, b) { }
-        public B(int a, int b,int d) : base(a , b)
+        public B(int a, int b, int d) : base(a, b)
         {
             this.d = d;
         }
-        private int d = 15;
-        public int C2 { get { if (true) { return C + d; } } }
+        private int d = -15;
+        public int C2 { get { if (d < 0) { d *= -1; }  return C + d;  } }
 
     }
 
@@ -33,11 +33,11 @@ namespace lab2
         static void Main(string[] args)
         {
             A objectA = new(10, 5);
-            Console.WriteLine($"Result{objectA.C}");
-            B objectB = new (20, 20);
-            Console.WriteLine($"Result{objectB.C2}");
-            B objectB2 = new (30,30,30);
-            Console.WriteLine($"Result{objectB2.C2}");
+            Console.WriteLine($"Result: {objectA.C}");
+            B objectB = new(20, 20);
+            Console.WriteLine($"Result: {objectB.C2}");
+            B objectB2 = new(30, 30, 30);
+            Console.WriteLine($"Result: {objectB2.C2}");
 
         }
     }
